d18c25776e77f2da068081268b55d23ea3e9827b
Jacob Gunnarsson <jacobgunnarsson@users.noreply.github.com>
2020-08-31 22:29:52 +0200

    Initial commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+  float speed;
+  float radius;
+  Vector2 direction;
+  // Start is called before the first frame update
+  void Start() {
+    speed = 1f;
+    radius = 10f;
+    direction = Vector2.one.normalized;
+  }
+
+  // Update is called once per frame
+  void Update() {
+    transform.Translate(direction * speed * Time.deltaTime);
+
+    if (transform.position.y + transform.localScale.y / 2 >= GameManager.topRight.y || transform.position.y + transform.localScale.y <= GameManager.bottomLeft.y) {
+      direction.y = direction.y * -1f;
+    }
+
+  }
+}
---
+++ Assets/Paddle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+  [SerializeField]
+  private float speed;
+  private float posY;
+  private string input;
+  private bool isPlayerPaddle;
+  // Start is called before the first frame update
+  void Start() {
+    posY = transform.localScale.y;
+    speed = 5f;
+  }
+
+  public void Init(bool thisIsPlayerPaddle) {
+    Vector2 pos;
+
+    isPlayerPaddle = thisIsPlayerPaddle;
+
+    if (isPlayerPaddle) {
+      pos = new Vector2(GameManager.bottomLeft.x + transform.localScale.x, 0);
+      input = "PlayerPaddle";
+      transform.name = "Player Paddle";
+    } else {
+      pos = new Vector2(GameManager.topRight.x - transform.localScale.x, 0);
+      transform.name = "AI Paddle";
+    }
+
+    transform.position = pos;
+  }
+
+  private bool isAllowedToMove(float move) {
+    return transform.position.y + move < GameManager.topRight.y - transform.localScale.y / 2 - transform.localScale.x / 2
+          && transform.position.y + move > GameManager.bottomLeft.y + transform.localScale.y / 2 + transform.localScale.x / 2;
+  }
+
+  // Update is called once per frame
+  void Update() {
+    if (isPlayerPaddle) {
+      float move = Input.GetAxis(input) * Time.deltaTime * speed;
+
+      if (isAllowedToMove(move)) {
+        transform.Translate(move * Vector2.up);
+      }
+    }
+  }
+}
